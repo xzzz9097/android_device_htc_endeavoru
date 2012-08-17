b40c3f4121a5a7bb52dd528d849c3adec2810a52
LorD ClockaN <davor@losinj.com>
2012-08-17 17:48:35 +0200

    build Power module again #blamexoomdev for https://github.com/AOKP/device_samsung_tuna/commit/e40108ea3fb03e89105a78028091e851ef7a5c76

--- full_endeavoru.mk
+++ full_endeavoru.mk
@@ -154,8 +154,8 @@ PRODUCT_PACKAGES += \
 	setup_fs
 
 # Power
-#PRODUCT_PACKAGES += \
-#        power.endeavoru
+PRODUCT_PACKAGES += \
+        power.endeavoru
 		
 # Permissions
 PRODUCT_COPY_FILES += \
