dc0cc00961cb8a397db1338937461033fc914cc8
LorD ClockaN <davor@losinj.com>
2013-04-13 19:30:53 +0200

    Update jpolly and pollyd from maxwen

--- jpolly/src/ch/blinkenlights/android/polly/PollyService.java
+++ jpolly/src/ch/blinkenlights/android/polly/PollyService.java
@@ -31,6 +31,17 @@ import android.media.AudioManager;
 import android.net.LocalSocket;
 import android.net.LocalSocketAddress;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.IllegalAccessException;
+import java.lang.IllegalArgumentException;
+import java.lang.ClassNotFoundException;
+import java.lang.NoSuchMethodException;
+import java.lang.reflect.Method;
+import java.lang.Exception;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 
 public class PollyService extends Service {
 	private final IBinder ply_binder        = new LocalBinder();
@@ -41,6 +52,9 @@ public class PollyService extends Service {
 	private AudioManager aMgr;
 	private TelephonyManager tMgr;
 	private int lastvol = -1;
+    private boolean inCall = false;
+    private static String TAG = "PollyService";
+    private static boolean boothackDone = false;
 	
 	@Override
 	public IBinder onBind(Intent i) {
@@ -52,16 +66,74 @@ public class PollyService extends Service {
 			return PollyService.this;
 		}
 	}
-	
+
 	@Override
 	public void onCreate() {
 		aMgr = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
 		tMgr = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
-		
+        // initial value
+    	lastvol = aMgr.getStreamVolume(aMgr.STREAM_VOICE_CALL);
+
+		// maxwen: service can be restarted and will send headset insert then
+		// which will cause troubles with apps listening to it
+        //if(isAudioVolumeHackRequired()){
+        //    applyAudioVolumeHack(); 
+        //}
+
 		registerReceiver(ply_receiver, new IntentFilter(INTENT_VOLUME));    /* undocumented */
 		registerReceiver(ply_receiver, new IntentFilter(INTENT_AIRPLANE));
 		registerReceiver(ply_receiver, new IntentFilter(INTENT_PHONESTATE));
+
 	}
+
+        private boolean isAudioVolumeHackRequired() {
+        	if(!boothackDone){
+                String value = fileReadOneLine("/sys/class/switch/h2w/state");
+                return value!=null && value.equals("0");
+            }
+            return false;
+        }
+
+        private void applyAudioVolumeHack() {
+                Log.d(TAG, "apply volume hack");
+                try {       
+                        Class paramString = String.class;
+	                Class paramInt = Integer.TYPE;
+
+                        Class c = Class.forName(aMgr.getClass().getName());  
+                        Method f = c.getDeclaredMethod("setWiredDeviceConnectionState", paramInt, paramInt, paramString); 
+                        f.setAccessible(true);  
+
+                        f.invoke(aMgr, 8, 1, new String("Headset"));
+                        f.invoke(aMgr, 8, 0, new String("Headset"));
+
+                        f.setAccessible(false);  
+                        boothackDone = true;
+
+                        Log.d(TAG,"apply volume hack success");
+                } catch (InvocationTargetException e) {
+                        Log.d(TAG,"apply volume hack failed - exception " + e.getTargetException());
+                } catch (Exception e) {
+                        Log.d(TAG,"apply volume hack - exception " + e);
+                } 
+        }
+
+        private String fileReadOneLine(String fname) {
+                BufferedReader br;
+                String line = null;
+
+                try {
+                        br = new BufferedReader(new FileReader(fname), 512);
+                        try {
+                                line = br.readLine();
+                        } finally {
+                                br.close();
+                        }
+                } catch (Exception e) {
+                        Log.e(TAG, "IO Exception when reading file "  + fname + " " + e);
+                }
+                return line;
+        }
 	
 	public void onDestroy() {
 		unregisterReceiver(ply_receiver);
@@ -74,7 +146,7 @@ public class PollyService extends Service {
 			
 			String ia = intent.getAction();
 			
-			// xlog("intent "+ia+" received");
+			xlog("intent "+ia+" received");
 			
 			if(ia == INTENT_VOLUME)     { updateIncallVolume();       }
 			if(ia == INTENT_PHONESTATE) { checkPhoneState();          }
@@ -89,16 +161,23 @@ public class PollyService extends Service {
 		private void updateIncallVolume() {
 			
 			/* android volumes go from 0-5
-			** but the modem expects 44-94
+			** but the modem expects 32-82
 			*/
 			int curvol    = aMgr.getStreamVolume(aMgr.STREAM_VOICE_CALL);
-			String vparam = "volo,40,8,3,"+(44+curvol*10);
 			
 			if(lastvol != curvol) {
+			    String vparam = "@volo,40,8,3,"+(32+curvol*10);
 				xlog("updating incall volume: "+vparam);
 				sendToPollySocket(vparam);
 				lastvol = curvol;
 			}
+		}
+
+		private void resetIncallVolume() {
+			String vparam = "@@";
+			
+                        xlog("reset incall volume: "+vparam);
+			sendToPollySocket(vparam);
 		}
 		
 		/**************************************************************
@@ -108,21 +187,14 @@ public class PollyService extends Service {
 		**************************************************************/
 		private void checkPhoneState() {
 			if(tMgr.getCallState() == tMgr.CALL_STATE_OFFHOOK) {
-				xlog("switched to INCALL mode - setting initial volume");
+				xlog("we are IN_CALL - forcing audio setting");
 				lastvol = -1;
 				updateIncallVolume();
-				
-				/* the modem may suffer from alzheimer's, a hacky
-				** solution is to sleep 1 second and send it again.
-				** I don't like this, but we have NO IDEA when the
-				** modem is ready. Thank you HTC.
-				*/
-				try {Thread.sleep(1000);}
-					catch (InterruptedException e) { xlog("Sleep was interrupted"); }
-				
-				xlog("sent incall volume a 2nd time");
-				lastvol = -1;
-				updateIncallVolume();
+                                inCall = true;
+			} else if(inCall && tMgr.getCallState() == tMgr.CALL_STATE_IDLE) {
+				xlog("we are END_CALL - reset audio setting");
+				resetIncallVolume();
+                                inCall = false;
 			}
 		}
 		
@@ -151,7 +223,9 @@ public class PollyService extends Service {
 				lsock.connect(new LocalSocketAddress(rild_socket, LocalSocketAddress.Namespace.FILESYSTEM));
 				lsock.getOutputStream().write(whatever.getBytes());
 			}
-			catch (Exception e) {}
+			catch (Exception e) {
+				xlog("sendToPollySocket "+e);
+            }
 			
 			try {
 				lsock.close();
@@ -163,9 +237,7 @@ public class PollyService extends Service {
 		** Send given string to android log                          **
 		**************************************************************/
 		private void xlog(String msg) {
-			Log.d("PollyService", msg);
+			Log.d(TAG, msg);
 		}
-		
-		
 	};
 }
