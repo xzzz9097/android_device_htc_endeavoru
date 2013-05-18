a9742800a386fa19fc24f00a22fadafa234481b0
LorD ClockaN <davor@losinj.com>
2013-05-18 15:19:05 +0200

    Move some build packages to common.mk

--- endeavoru.mk
+++ endeavoru.mk
@@ -114,10 +114,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/media_codecs.xml:system/etc/media_codecs.xml
 
-# bluetooth config
-PRODUCT_COPY_FILES += \
-    system/bluetooth/data/main.conf:system/etc/bluetooth/main.conf
-
 # configs
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/config/enctune.conf:system/etc/enctune.conf \
@@ -209,7 +205,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     EndeavoruParts
 
-
 # Wi-Fi
 PRODUCT_PACKAGES += \
     dhcpcd.conf \
@@ -220,16 +215,5 @@ PRODUCT_PACKAGES += \
     regulatory.bin \
     calibrator \
     wlconf
-
-# Custom Packages
-PRODUCT_PACKAGES += \
-        Music \
-        CMFileManager \
-        CellBroadcastReceiver \
-        Apollo \
-        DSPManager \
-        libcyanogen-dsp \
-        audio_effects.conf \
-        JellyBlue_X
 
 $(call inherit-product-if-exists, vendor/htc/endeavoru/endeavoru-vendor.mk)
