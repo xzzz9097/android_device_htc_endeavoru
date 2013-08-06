df8d68a8b6cd1ae8384b36a876b59cbe19cb0e96
Thomas Wendt <thoemy@gmx.net>
2013-08-06 12:56:11 +0200

    endeavoru: unify fstab
    Change-Id: Ie544e121c8e01d957aaa4d7a381e7fd64da7b75c

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -170,6 +170,8 @@ BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/htc/endeavoru/bluetooth
 # BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_15x24.h\"
 BOARD_UMS_LUNFILE := "/sys/devices/platform/fsl-tegra-udc/gadget/lun0/file"
 BOARD_HAS_NO_SELECT_BUTTON := true
+TARGET_RECOVERY_FSTAB := device/htc/endeavoru/ramdisk/fstab.endeavoru
+RECOVERY_FSTAB_VERSION := 2
 
 # Releasetools
 TARGET_RELEASETOOLS_EXTENSIONS := device/htc/endeavoru
